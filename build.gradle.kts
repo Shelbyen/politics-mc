699b12dfe855d93be51c799d9eafd69de513f2bf
Shelbyen <ars.sheberstov@mail.ru>
2025-01-31 01:51:36 +0500

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+}
+
+group = "funn.j2k"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/funn/j2k/politicsMc/PoliticsMc.kt
@@ -0,0 +1,15 @@
+package funn.j2k.politicsMc
+
+import org.bukkit.plugin.java.JavaPlugin
+
+
+class PoliticsMc : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
