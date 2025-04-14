5261dfcfd94fc9da5f2c9c8210f58eca77374a28
Shelbyen <ars.sheberstov@mail.ru>
2025-04-14 15:11:15 +0500

    add some utilities

---
+++ src/main/kotlin/funn/j2k/politicsMc/utilities/customEntities.kt
@@ -0,0 +1,25 @@
+package funn.j2k.politicsMc.utilities
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Entity
+
+class EntityTag(
+    private val tag: String
+)  {
+    fun getEntities() = allEntities().filter { it.scoreboardTags.contains(tag) }
+
+    fun onTick(action: (Entity) -> Unit) {
+        funn.j2k.politicsMc.utilities.onTick {
+            getEntities().forEach { action(it) }
+        }
+    }
+
+    fun onInteract(action: (event: org.bukkit.event.player.PlayerInteractEntityEvent) -> Unit) {
+        onInteractEntity { event ->
+            if (!event.rightClicked.scoreboardTags.contains(tag)) return@onInteractEntity
+            action(event)
+        }
+    }
+}
+
+fun allEntities() = Bukkit.getServer().worlds.flatMap { it.entities }
---
+++ src/main/kotlin/funn/j2k/politicsMc/utilities/rendering/utilities.kt
@@ -0,0 +1,20 @@
+package funn.j2k.politicsMc.utilities.rendering
+
+import org.bukkit.entity.Display
+import org.bukkit.util.Transformation
+import org.joml.Matrix4f
+
+
+fun Display.interpolateTransform(transformation: Transformation) {
+    if (this.transformation == transformation) return
+    this.transformation = transformation
+    this.interpolationDelay = 0
+}
+
+fun Display.interpolateTransform(matrix: Matrix4f) {
+    val oldTransform = this.transformation
+    setTransformationMatrix(matrix)
+
+    if (oldTransform == this.transformation) return
+    this.interpolationDelay = 0
+}
