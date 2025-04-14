7f5f91378042ab598e145402a6f4039c8ab1505a
Shelbyen <ars.sheberstov@mail.ru>
2025-04-14 19:36:13 +0500

    refactor: took map to a separate class

--- src/main/kotlin/funn/j2k/politicsMc/custom_map/maps/PerlinNoiseMap.kt
+++ src/main/kotlin/funn/j2k/politicsMc/custom_map/maps/PerlinNoiseMap.kt
@@ -1,26 +1,16 @@
 package funn.j2k.politicsMc.custom_map.maps
 
 import funn.j2k.politicsMc.custom_map.bitmapToRenderEntities
-import funn.j2k.politicsMc.utilities.*
 import funn.j2k.politicsMc.utilities.rendering.SharedEntityRenderer
 import org.bukkit.Color
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
 import org.bukkit.util.noise.SimplexNoiseGenerator
-import org.joml.Quaterniond
-import org.joml.Quaternionf
-import org.joml.Vector3f
 
 
 class PerlinNoiseMap(
-    private val player: Player,
-    private val localPosition: Vector
-) : Map {
-    private val size = 16
-    private val bitmap = Grid(size, size) { CLEAR_COLOR }
-    private var lastChunkPosition = player.chunk
-    override fun getMap() = bitmap
-
+    player: Player, localPosition: Vector = Vector(0.8f, -0.4f, 1f)
+) : Map(player, localPosition) {
     init {
         updateMap()
     }
@@ -29,19 +19,10 @@ class PerlinNoiseMap(
         for ((px, py) in bitmap.indices()) {
             val color = ((SimplexNoiseGenerator.getInstance().noise(
                 (player.chunk.x - size / 2 + px).toDouble(),
-                (player.chunk.z - size / 2 + py).toDouble()) + 1) * 255 / 2).toInt()
+                (player.chunk.z - size / 2 + py).toDouble()
+            ) + 1) * 255 / 2).toInt()
             bitmap[px to py] = Color.fromRGB(color, color, color)
         }
-    }
-
-    private fun calculatePosition(): Vector {
-        val localPositionRotate = Vector(localPosition.x, localPosition.y, localPosition.z).rotate(
-            Quaterniond().rotateYXZ(
-                player.eyeLocation.yawRadians().toDouble(),
-                0.0, 0.0
-            )
-        )
-        return (player.eyeLocation.toVector().add(localPositionRotate))
     }
 
     override fun update() {
@@ -50,13 +31,13 @@ class PerlinNoiseMap(
             lastChunkPosition = player.chunk
         }
 
-        val quaternion = Quaternionf().rotateAxis(player.eyeLocation.yawRadians() + Math.toRadians(180.0).toFloat(), Vector3f(0f, 1f, 0f))
+        val posValues = calculatePosition()
 
         SharedEntityRenderer.render(
             this, bitmapToRenderEntities(
                 world = player.world,
-                position = calculatePosition(),
-                quaternion = quaternion,
+                position = posValues.first,
+                quaternion = posValues.second,
                 bitmap = getMap()
             )
         )
--- src/main/kotlin/funn/j2k/politicsMc/custom_map/maps/maps.kt
+++ src/main/kotlin/funn/j2k/politicsMc/custom_map/maps/maps.kt
@@ -3,16 +3,49 @@ package funn.j2k.politicsMc.custom_map.maps
 import funn.j2k.politicsMc.custom_map.FragmentData
 import funn.j2k.politicsMc.custom_map.RenderBuffer
 import funn.j2k.politicsMc.utilities.Grid
+import funn.j2k.politicsMc.utilities.rotate
+import funn.j2k.politicsMc.utilities.yawRadians
 import org.bukkit.Color
+import org.bukkit.entity.Player
+import org.bukkit.util.Vector
+import org.joml.Quaterniond
+import org.joml.Quaternionf
+import org.joml.Vector3f
 
 val CLEAR_COLOR = Color.fromARGB(50, 0, 0, 0)
 
-interface Map {
+interface Bitmap {
     fun getMap(): Grid<Color>
     fun update() {}
 }
 
-class EmptyMap : Map {
+
+abstract class Map(
+    val player: Player,
+    private val localPosition: Vector = Vector(0.8f, -0.4f, 1f)
+) : Bitmap {
+    protected val size = 16
+    protected val bitmap = Grid(size, size) { CLEAR_COLOR }
+    protected var lastChunkPosition = player.chunk
+    override fun getMap() = bitmap
+
+    protected fun calculatePosition(): Pair<Vector, Quaternionf> {
+        val localPositionRotate = Vector(localPosition.x, localPosition.y, localPosition.z).rotate(
+            Quaterniond().rotateYXZ(
+                player.eyeLocation.yawRadians().toDouble(),
+                0.0, 0.0
+            )
+        )
+        val quaternion = Quaternionf().rotateAxis(
+            player.eyeLocation.yawRadians() + Math.toRadians(180.0).toFloat(),
+            Vector3f(0f, 1f, 0f)
+        )
+        return Pair(player.eyeLocation.toVector().add(localPositionRotate), quaternion)
+    }
+}
+
+
+class EmptyMap(player: Player, localPosition: Vector) : Map(player, localPosition) {
     private val buffer = RenderBuffer(64, 64) { FragmentData(CLEAR_COLOR) }
     override fun getMap() = buffer.map { it.color }
 }
